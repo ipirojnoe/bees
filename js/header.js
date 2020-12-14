e3442e5d78501ae2e331b217915fd2370a8936de
Ilya <deminiiii@gmail.com>
2020-12-14 23:29:57 +0300

    Style responsive menu

--- js/header.js
+++ js/header.js
@@ -1,9 +1,10 @@
 $(function(){
   $('.header-nav-link').click(function(){
     var id = $(this).attr('href');
+    var offSet = window.screen.availWidth <= 640 ? 0 : 72;
 
     $('html, body').animate({
-      scrollTop: ($(id).offset().top - 72)
+      scrollTop: ($(id).offset().top - offSet)
     }, 500);
     return false;
   });
